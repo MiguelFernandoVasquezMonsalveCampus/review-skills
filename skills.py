0c63b7bbef81162906b6ffc95569b16ba677660d
Miguel Vasquez <39173126+MiguelFernandoVasquezMonsalveCampus@users.noreply.github.com>
2023-02-16 06:34:03 -0500

    base

---
+++ skills.py
@@ -0,0 +1,55 @@
+def skill(x):
+    x = int(input('Digite una opcion para el ejercicio: '))
+    if x == 1:
+        ej1()
+    elif x == 2:
+        ej2()
+    elif x == 3:
+        ej2()
+    elif x == 4:
+        ej2()
+    elif x == 5:
+        ej2()
+    elif x == 6:
+        ej2()
+    elif x == 7:
+        ej2()
+    elif x == 8:
+        ej2()
+    elif x == 9:
+        ej2()
+    elif x == 10:
+        ej2()
+
+
+
+def ej1():
+    """"""
+
+def ej2():
+    """"""
+
+def ej3():
+    """"""
+
+def ej4():
+    """"""
+
+def ej5():
+    """"""
+
+def ej6():
+    """"""
+
+def ej7():
+    """"""
+
+def ej8():
+    """"""
+
+def ej9():
+    """"""
+
+def ej10():
+    """"""
+
