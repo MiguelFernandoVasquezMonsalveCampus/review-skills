6a86ea26e9b89fcceb5610f4162c9278070e53de
Miguel Vasquez <39173126+MiguelFernandoVasquezMonsalveCampus@users.noreply.github.com>
2023-02-16 08:50:06 -0500

    ejercicio 1

--- skills.py
+++ skills.py
@@ -1,5 +1,50 @@
-def skill(x):
-    x = int(input('Digite una opcion para el ejercicio: '))
+def ej1():
+    """ 1. Qué operadores utiliza Python en los siguientes casos:
+
+    A. División Modular
+    B. Exponenciación
+    C. División que retorne entero. """
+
+    num1= 6
+    num2= 4
+
+    print(f'A. División Modular {num1} // {num2} = {num1 // num2}\nB. Exponenciacion {num1} ** {num2} = {num1 ** num2}\nC. División que retorne entero. {num1} // {num2} = {num1 // num2}')
+ 
+
+def ej2():
+    """"""
+ 
+
+def ej3():
+    """"""
+def ej4():
+    """"""
+
+def ej5():
+    """"""
+
+def ej6():
+    """"""
+
+def ej7():
+    """"""
+
+def ej8():
+    """"""
+
+def ej9():
+    """"""
+
+def ej10():
+    """"""
+
+
+
+
+
+#------------------------------------Menu------------------------------------
+def skill():
+    x = int(input('[1] ejercicio \nejercicio [2]\nejercicio [3]\nejercicio [4]\nejercicio [5]\nejercicio [6]\nejercicio [7]\nejercicio [8]\nejercicio [9]\nejercicio [10]\nDigite una opcion para el ejercicio: '))
     if x == 1:
         ej1()
     elif x == 2:
@@ -22,34 +67,7 @@ def skill(x):
         ej2()
 
 
+skill()
 
-def ej1():
-    """"""
 
-def ej2():
-    """"""
-
-def ej3():
-    """"""
-
-def ej4():
-    """"""
-
-def ej5():
-    """"""
-
-def ej6():
-    """"""
-
-def ej7():
-    """"""
-
-def ej8():
-    """"""
-
-def ej9():
-    """"""
-
-def ej10():
-    """"""
 
