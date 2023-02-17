9e1b8e096cb28ac108c91347db1caaf0995e6c08
Miguel Vasquez <miguelvasquezf8@gmail.com>
2023-02-17 09:18:44 -0500

    no termine el 3 y no etendi  el 3

---
+++ campus3.py
@@ -0,0 +1,19 @@
+""" 3. En pocos días comienza la vuelta a España y la federación
+colombiana de ciclismo, como incentivo ha determinado pagar
+un valor adicional. El programa pedirá por teclado el sueldo
+básico por kilometro recorrido, el número de kilómetros
+recorridos durante toda la vuelta, numero de kilómetros
+recorridos con la camiseta de líder.
+Calcular el valor a pagar total, si se sabe que si recorre en la
+bici más de 1800 kilómetros con la camiseta de líder, esos
+kilómetros se consideran especiales y tendrán un recargo de
+25%.
+El total de kilómetros por recorrer durante toda la vuelta serán
+3.277 kilómetros,el ganador de la vuelta a España recibirá 700
+millones de pesos. """
+
+ciclista1 = int(input('Ingrese sus kilometros ciclista #1 '))
+ciclista2 = int(input('Ingrese sus kilometros ciclista #2 '))
+ciclista3 = int(input('Ingrese sus kilometros ciclista #3 '))
+ciclista4 = int(input('Ingrese sus kilometros ciclista #4 '))
+
---
+++ campus4.py
@@ -0,0 +1,5 @@
+import random
+import json
+Almacen = []
+lista = {}
+articulo = []
